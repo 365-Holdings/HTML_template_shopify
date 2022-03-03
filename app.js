89f006cf0977a959fe65178fff048d3d246251c9
Derek Thorpe <derekthorpe@MacBook-Pro-5.local>
2022-03-03 09:06:20 -0500

    html template

---
+++ app.js
@@ -0,0 +1,24 @@
+// Script for HTML template
+// =====================================================
+
+$(document).ready(function(){
+    $(".accordion h3").click(function(){
+    var id = this.id;   /* getting heading id */
+
+/* looping through all elements which have class .accordion-content */
+    $(".accordion-content").each(function(){             
+        
+        if($("#"+id).next()[0].id != this.id){
+            $(this).slideUp();
+}
+      
+     
+});
+  
+      
+
+$(this).next().toggle();  /* Selecting div after h3 */
+
+      
+});
+});
